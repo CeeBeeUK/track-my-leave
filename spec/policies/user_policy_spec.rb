d50820fd67455feb6e1c58c3635ca17de23e8356
CeeBeeUK <colinbruce@gmail.com>
2016-09-19 21:38:02 +0100

    Add user policy
    Only allow users to view own policy

---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe UserPolicy, type: :policy do
+  let(:accessing_user) { build_stubbed(:user) }
+
+  subject(:policy) { described_class.new(accessing_user, subject_user) }
+
+  context 'when the user views their own profile' do
+    let(:subject_user) { accessing_user }
+    it { is_expected.to permit_action(:show) }
+  end
+
+  context 'when the user tries to view another users profile' do
+    let(:subject_user) { build_stubbed(:user) }
+
+    it { is_expected.not_to permit_action(:show) }
+  end
+end
