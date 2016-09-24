9d7ca4bc502102283e1a641d136a4a1c9626938f
CeeBeeUK <colinbruce@gmail.com>
2016-09-24 15:08:27 +0100

    add current_for method

--- app/models/leave_year.rb
+++ app/models/leave_year.rb
@@ -2,4 +2,8 @@ class LeaveYear < ActiveRecord::Base
   belongs_to :user, required: true
 
   validates :starts_on, :entitlement, :carried_forward, presence: true
+
+  def self.current_for(user)
+    LeaveYear.find_by(user: user)
+  end
 end
--- spec/models/leave_year_spec.rb
+++ spec/models/leave_year_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe LeaveYear, type: :model do
     it { is_expected.to validate_presence_of :entitlement }
     it { is_expected.to validate_presence_of :carried_forward }
   end
+
+  describe '#current_for' do
+    let!(:user) { create :user }
+
+    subject { described_class.current_for(user) }
+
+    context 'when passed a user with a leave year' do
+      let!(:leave_year) { create :leave_year, user: user }
+      it { is_expected.to eq leave_year }
+    end
+
+    context 'when passed a user with no leave year' do
+      let!(:leave_year) { nil }
+      it { is_expected.to eq leave_year }
+    end
+  end
 end
